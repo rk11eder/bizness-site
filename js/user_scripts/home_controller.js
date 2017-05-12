4d3a8cfc210ebd714ddd011abe1e866aa394d2c2
Celso Madeira <celso.madeira@gmail.com>
2017-05-12 12:40:37 +0100

    acertos canvas

--- js/user_scripts/home_controller.js
+++ js/user_scripts/home_controller.js
@@ -13,9 +13,50 @@ arquitetaApp.controller('homeCtrl', ['$scope', '$rootScope','$window','$timeout'
   	PageTitle.setTitle('PROJECTSTART');
   	PageTitle.setDesc($rootScope.lang_array.descricao_page);
    
-$scope.ins = items.data;
+/*$scope.ins = items.data;*/
+
+/*
+  console.log($scope.ins);*/
+  /*RESIZE WINDOW*/
+	var w = angular.element($window);
+	$scope.getWindowDimensions = function () {
+		return {
+		   'h': w.height(),
+		   'w': w.width()
+		};
+	};
+
+   var canvas = document.querySelector('canvas');
+
+  canvas.width = w.innerWidth();
+	canvas.height = w.innerHeight();
+
+  var c = canvas.getContext('2d');
+/*	c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+  c.fillRect(100, 100, 100, 100);*/
+
+/*	for(var i = 0; i < 3; i++){
+		
+    var x = Math.random() * w.innerWidth();
+		
+    var y = Math.random() * w.innerHeight();
+		
+    c.beginPath();
+		c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    c.arc(x, y, 30, 0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.stroke();
+}*/
+
+		c.beginPath();
+		c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    c.arc(200, 200, 30, 0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.stroke();
 
 
-  console.log($scope.ins);
+
+  console.log(canvas);
+  
 
 }]);
