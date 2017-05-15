67fc06c146cb0b22bd3a2c050f0082ccd03d1622
Celso Madeira <celso.madeira@gmail.com>
2017-05-15 10:37:07 +0100

    acertos canvas

--- js/script.js
+++ js/script.js
@@ -193,10 +193,11 @@ arquitetaApp.controller('homeCtrl', ['$scope', '$rootScope','$window','$timeout'
 
    var canvas = document.querySelector('canvas');
 
-  canvas.width = w.innerWidth();
-	canvas.height = w.innerHeight();
+  canvas.width = 1000;
+	canvas.height = 700;
 
   var c = canvas.getContext('2d');
+	var img = document.getElementById("nigga");
 /*	c.fillStyle = 'rgba(255, 0, 0, 0.5)';
   c.fillRect(100, 100, 100, 100);*/
 
@@ -213,13 +214,144 @@ arquitetaApp.controller('homeCtrl', ['$scope', '$rootScope','$window','$timeout'
 		c.stroke();
 }*/
 
-		c.beginPath();
+	/*	c.beginPath();
 		c.fillStyle = 'rgba(255, 0, 0, 0.5)';
     c.arc(200, 200, 30, 0, Math.PI * 2, false);
 		c.strokeStyle = 'blue';
-		c.stroke();
+		c.stroke();*/
+
+	function Circle(x,y,dx,dy,radius){
+			this.x = x;
+			this.y = y;
+			this.dx = dx;
+			this.dy = dy;
+			this.radius = radius;
+
+			this.draw = function(){
+				c.beginPath();
+				c.arc(this.x,this.y,this.radius,0,Math.PI * 2, false);
+				c.strokeStyle = 'blue';
+				c.stroke();
+			}
+
+			this.update = function(){
+					if(this.x + this.radius > innerWidth || this.x -  this.radius <0){
+						this.dx = -this.dx;
+					}
+
+					if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+						this.dy = -this.dy;
+					}
+
+					this.x += this.dx;
+					this.y += this.dy;
+
+					this.draw();
+			}		
+	}	
+
+	/*var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var radius = 30;*/
+/*var circle = new Circle(200, 200, 3, 3,30);*/
+var circleArray = [];
+
+	for(var i = 0; i < 50; i++){
+			var x = Math.random() * innerWidth;
+			var y = Math.random() * innerHeight;
+			var dx = (Math.random() - 0.5) * 8;
+			var dy = (Math.random() - 0.5) * 8;
+			var radius = 30;
+			circleArray.push(new Circle(x,y,dx,dy,radius));
+			
+	}
 
 
+	function Quad(x,y,z,t,dx,dy){
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.t = t;
+			this.dx = dx;
+			this.dy = dy;
+			
+
+			this.draw = function(){
+				c.beginPath();
+				c.rect(this.x,this.y,this.z,this.t);	
+				c.strokeStyle = 'blue';
+				c.stroke();
+			}
+
+			this.update = function(){
+					if(this.x - 20 > 1000 || this.x <0){
+						this.dx = -this.dx;
+					}
+					
+
+					if(this.y > 700 || this.y < 0){
+						this.dy = -this.dy;
+					}
+
+					this.x += this.dx;
+					this.y += this.dy;
+
+					this.draw();
+			}		
+	}	
+
+	/*var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var radius = 30;*/
+/*var circle = new Circle(200, 200, 3, 3,30);*/
+var quadArray = [];
+
+	for(var i = 0; i < 50; i++){
+			var x = Math.random() * canvas.width;
+			var y = Math.random() * canvas.height;
+			var z = 20;
+			var t = 20;
+			var dx = (Math.random() - 0.5) * 2;
+			var dy = (Math.random() - 0.5) * 2;
+			quadArray.push(new Quad(x,y,z,t,dx,dy));
+			
+	}
+
+
+
+/*	// Filled triangle
+    c.beginPath();
+    c.moveTo(25, 25);
+    c.lineTo(105, 25);
+    c.lineTo(25, 105);
+    c.fill();*/
+
+console.log(canvas.width);
+
+		
+
+
+		function animate(){
+			requestAnimationFrame(animate);
+
+			c.clearRect(0, 0 , innerWidth, innerHeight);
+			
+
+		/*	for (var i = 0; i < circleArray.length; i++){
+					circleArray[i].update();
+			}*/
+
+			for (var i = 0; i < quadArray.length; i++){
+					quadArray[i].update();
+			}
+			
+		}
+
+		animate();
 
   console.log(canvas);
   
