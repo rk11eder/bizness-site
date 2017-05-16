edbb17f932b68a7a9dff086b86cbedc15d947db5
Celso Madeira <celso.madeira@gmail.com>
2017-05-16 18:14:03 +0100

    parallax

--- js/user_scripts/portfolio2_controller.js
+++ js/user_scripts/portfolio2_controller.js
@@ -13,7 +13,38 @@ arquitetaApp.controller('portfolio2Ctrl', ['$scope', '$rootScope','$window','$ti
   	PageTitle.setTitle('PROJECTSTART');
   	PageTitle.setDesc($rootScope.lang_array.descricao_page);
    
- 
+		angular.element('body').bind("scroll", function(){
+
+				var portfolio = $('.holder_backgrounds_portfolio2').offset().top;
+				var currentScroll = $('.holder_backgrounds_portfolio2').offset().top; // get current position
+				console.log(currentScroll);
+
+				$scope.map = function(x, in_min, in_max, out_min, out_max){
+					if(x<in_min) return out_min;
+					if(x>in_max) return out_max;
+					return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+				}
+
+				//PERGUNTAR JOAO SE ISTO PODE FICAR ASSIM "PERFORMANCE"
+				$scope.valor =	$scope.map(portfolio, -400, 0, 0.3, 0.9);
+				$('.holder_backgrounds_portfolio2').css('opacity', $scope.valor);
+
+						var conteudo = $('.conteudo_port_holder').offset().top;
+						
+						
+						var parallaximage = $('.folio2_header');
+
+				parallaximage.css('transform','translateY('+ -(currentScroll * .5) + 'px)' );
+					
+
+
+
+
+            $scope.$apply();
+        });
+
+			
+			
   
 
 }]);
