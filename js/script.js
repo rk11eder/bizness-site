f9afd0b458d5c3a70ddc1a4b385e2819096458a0
luis silva <luissilva@spic.pt>
2017-05-17 12:19:20 +0100

    acerto

--- js/script.js
+++ js/script.js
@@ -702,26 +702,6 @@ $rootScope.hide_menu = 0;
 'use strict';
 
 /*exemplo controller*/
-arquitetaApp.controller('portfolio2Ctrl', ['$scope', '$rootScope','$window','$timeout','$sce','items','services','$routeParams','PageTitle','$location','$http', function portfolio2Ctrl($scope, $rootScope, $window,$timeout,$sce,items, services,$routeParams,PageTitle,$location,$http){
-
-	/*CLICKS GOOGLE MAPS*/
-  $scope.$on('$viewContentLoaded', function(event) {
-    $window.ga('send', 'pageview', { page: $location.url() });  
-  });
-
-		
-	/*SET TITLE PAGE SEO*/
-  	PageTitle.setTitle('PROJECTSTART');
-  	PageTitle.setDesc($rootScope.lang_array.descricao_page);
-    $rootScope.hide_menu = 0;
-    $rootScope.hide_botao_menu = 0;
-
-
-  
-
-}]);'use strict';
-
-/*exemplo controller*/
 arquitetaApp.controller('portfolioCtrl', ['$scope', '$rootScope','$window','$timeout','$sce','items','services','$routeParams','PageTitle','$location','$http', function portfolioCtrl($scope, $rootScope, $window,$timeout,$sce,items, services,$routeParams,PageTitle,$location,$http){
 
 	/*CLICKS GOOGLE MAPS*/
