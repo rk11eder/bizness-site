5cb2c7bc7a0ebd54758d8892ef3aad38f4c98e0a
Eder Barbosa <eder@spic.pt>
2017-05-26 12:42:45 +0100

    get data

--- server/portfolio_get_data.php
+++ server/portfolio_get_data.php
@@ -15,17 +15,17 @@ $database = new database();
 
 
 
-/*$res_select_projectos = $database->query_simple_prepare("SELECT projetos.id, projetos.activo, projetos.logo, projetos.cor, projetos.destaque, projetos_fotos.id, projetos_foto.foto FROM ".$database->array_tables[1].", ".$database->array_tables[2]. "WHERE " .$database->array_tables[1].".id" ."=". $database->array_tables[2].".id_projeto"  ,array(),"");*/
+$res_select_projectos = $database->query_simple_prepare("SELECT projetos.id, projetos.activo, projetos.logo, projetos.cor, projetos.destaque, projetos_foto.foto FROM ".$database->array_tables[1].", ".$database->array_tables[2]." WHERE " .$database->array_tables[1].".id" ."=". $database->array_tables[2].".id_projeto",array(),"");
 
-$res = $database->query_simple_prepare("SELECT 
-    projetos.id, projetos.activo, projetos.logo, projetos.cor, projetos.destaque, projetos_fotos.id, projetos_foto.foto
- 	FROM 
- 	projetos, 
- 	projetos_fotos 
- 	WHERE 
- 	projetos.id = projetos_fotos.id_destaque
- 	
-	",array(),"");
+//$res = $database->query_simple_prepare("SELECT
+//    projetos.id, projetos.activo, projetos.logo, projetos.cor, projetos.destaque, projetos_fotos.id, projetos_foto.foto
+// 	FROM
+// 	projetos,
+// 	projetos_fotos
+// 	WHERE
+// 	projetos.id = projetos_fotos.id_destaque
+//
+//	",array(),"");
 
 echo json_encode($res);
 
