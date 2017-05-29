9a52b19cadcc6b6271910750f5999cd1ae4bcd48
Celso Madeira <celso.madeira@gmail.com>
2017-05-29 12:25:20 +0100

    acertos

--- js/user_scripts/sobrenos2_controller.js
+++ js/user_scripts/sobrenos2_controller.js
@@ -13,15 +13,6 @@ biznessApp.controller('sobrenosCtrl', ['$scope', '$rootScope','$window','$timeou
   	PageTitle.setTitle('PROJECTSTART');
   	PageTitle.setDesc($rootScope.lang_array.descricao_page);
     $scope.ins = items.data;
-   
-/*RESIZE WINDOW*/
-	var w = angular.element($window);
-	$scope.getWindowDimensions = function () {
-		return {
-		   'h': w.height(),
-		   'w': w.width()
-		};
-	};
 
    
 
@@ -38,61 +29,66 @@ biznessApp.controller('sobrenosCtrl', ['$scope', '$rootScope','$window','$timeou
 
 
 	$scope.resize_func = function(){
-		var canvas = document.querySelector('canvas');
+	var canvas = document.querySelector('canvas');
 
-		canvas.width = document.body.clientWidth;
-		canvas.height = w.innerHeight() * 0.95;
-		var c = canvas.getContext('2d');
 
-		var canvasPos = getPosition(canvas);
-		var mouseX = 0;
-		var mouseY = 0;
-		var sqSize = 100;
-		var xPos = 0;
-		var yPos = 0;
-		var dX = 0;
-		var dY = 0;
+  	canvas.width = document.body.clientWidth;
+	// canvas.height = w.height() - (w.height() * 0.10);
+	canvas.height = w.innerHeight() * 0.95;
 
-		canvas.addEventListener("mousemove", setMousePosition, false);
- 
-		function setMousePosition(e) {
-		mouseX = e.clientX - canvasPos.x;
-		mouseY = e.clientY - canvasPos.y;
-	}
 	
-	window.addEventListener("scroll", updatePosition, false);
-	window.addEventListener("resize", updatePosition, false);
 
-	function updatePosition() {
-  		canvasPos = getPosition(canvas);
+	var c = canvas.getContext('2d');
+
+	
+
+	function Circle(x,y,dx,dy,radius){
+			this.x = x;
+			this.y = y;
+			this.dx = dx;
+			this.dy = dy;
+			this.radius = radius;
+
+			this.draw = function(){
+				c.beginPath();
+				c.arc(this.x,this.y,this.radius,0,Math.PI * 2, false);
+				c.strokeStyle = 'blue';
+				c.stroke();
+			}
+
+			this.update = function(){
+					if(this.x + this.radius > innerWidth || this.x -  this.radius <0){
+						this.dx = -this.dx;
+					}
+
+					if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
+						this.dy = -this.dy;
+					}
+
+					this.x += this.dx;
+					this.y += this.dy;
+
+					this.draw();
+			}		
+	}	
+
+	/*var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var radius = 30;*/
+/*var circle = new Circle(200, 200, 3, 3,30);*/
+var circleArray = [];
+
+	for(var i = 0; i < 50; i++){
+			var x = Math.random() * innerWidth;
+			var y = Math.random() * innerHeight;
+			var dx = (Math.random() - 0.5) * 8;
+			var dy = (Math.random() - 0.5) * 8;
+			var radius = 30;
+			circleArray.push(new Circle(x,y,dx,dy,radius));
+			
 	}
-
-	// Helper function to get an element's exact position
-function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
- 
-  while (el) {
-    if (el.tagName == "BODY") {
-      // deal with browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
- 
-      xPos += (el.offsetLeft - xScroll + el.clientLeft);
-      yPos += (el.offsetTop - yScroll + el.clientTop);
-    } else {
-      // for all other non-BODY elements
-      xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-      yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-    }
- 
-    el = el.offsetParent;
-  }
-  return {
-    x: xPos,
-    y: yPos
-  };
-}   
 
 
 	function Quad(x,y,z,t,dx,dy){
@@ -113,10 +109,12 @@ function getPosition(el) {
 			}
 
 			this.update = function(){
-
-					
+				/*var ratox = event.clientX - canvas.getBoundingClientRect().left;
+				var ratoy = event.clientY - canvas.getBoundingClientRect().top;
+				console.log(ratox+"ratox");*/
+				/*var distance = Math.sqrt((mouseX - this.x)^2 + (mouseY - this.y)^2 );*/
 				
-					if(this.x + 10 > canvas.width || this.x <0){
+					/*if(this.x + 10 > canvas.width || this.x <0){
 						this.dx = -this.dx;
 					}
 					
@@ -125,20 +123,19 @@ function getPosition(el) {
 						this.dy = -this.dy;
 					}
 
-					this.dx = mouseX - this.x;
-					this.dy = mouseY - this.y;
-
-					this.x += (this.dx / 1000);
-					this.y += (this.dy / 1000);
-
-					/*this.x += this.dx;
+					this.x += this.dx;
 					this.y += this.dy;*/
 
 					this.draw();
 			}		
 	}	
 
-
+	/*var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var radius = 30;*/
+/*var circle = new Circle(200, 200, 3, 3,30);*/
 var quadArray = [];
 
 	for(var i = 0; i < 20; i++){
@@ -146,12 +143,64 @@ var quadArray = [];
 			var y = Math.random() * (canvas.height - 10);
 			var z = 10;
 			var t = 10;
-			var dx = (Math.random() - 0.5) * 0.5;
-			var dy = (Math.random() - 0.5) * 0.5;
+			/*var dx = (Math.random() - 0.5) * 2;
+			var dy = (Math.random() - 0.5) * 2;*/
 			quadArray.push(new Quad(x,y,z,t,dx,dy));
 			
 	}
 
+	function Tri(x,y,h,dx,dy){
+			this.x = x;
+			this.y = y;
+			this.h = h;
+			this.dx = dx;
+			this.dy = dy;
+			
+
+			this.draw = function(){
+				c.beginPath();
+				c.moveTo(this.x, this.y );
+				c.lineTo(this.x - this.h/2,this.y + this.h/2*Math.tan(60*Math.PI/180));
+				c.lineTo(this.x + this.h/2, this.y + this.h/2*Math.tan(60*Math.PI/180));
+				c.fillStyle = "#34394f";
+				c.fill();
+			}
+
+			this.update = function(){
+					/*if(this.x > (canvas.width - this.h/2)|| this.x <0){
+						this.dx = -this.dx 
+					}
+
+					if( this.y + this.h/2 > canvas.height  || this.y < 0){
+						this.dy = -this.dy;
+					}
+
+					this.x += this.dx;
+					this.y += this.dy;*/
+				
+
+					this.draw();
+			}		
+	}	
+
+	/*var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+	var dx = (Math.random() - 0.5) * 8;
+	var dy = (Math.random() - 0.5) * 8;
+	var radius = 30;*/
+/*var circle = new Circle(200, 200, 3, 3,30);*/
+var triArray = [];
+	
+	for(var i = 0; i < 20; i++){
+			
+			var x = Math.random() * canvas.width;
+			var y = Math.random() * canvas.height;
+			var h = 10;
+			/*var dx = (Math.random() - 0.5) * 2;
+			var dy = (Math.random() - 0.5) * 2;*/
+			triArray.push(new Tri(x,y,h,dx,dy));
+			
+	}
 
 function Trirot(x,y,a,d,dx,dy){
 			this.x = x;
@@ -196,10 +245,7 @@ function Trirot(x,y,a,d,dx,dy){
 			}
 
 			this.update = function(){
-					
-
-
-					if(this.x > canvas.width - this.d || this.x < this.d){
+					/*if(this.x > canvas.width - this.d || this.x < this.d){
 						this.dx = -this.dx 
 					}
 
@@ -208,7 +254,7 @@ function Trirot(x,y,a,d,dx,dy){
 					}
 
 					this.x += this.dx;
-					this.y += this.dy;
+					this.y += this.dy;*/
 				
 
 					this.draw();
@@ -221,18 +267,24 @@ function Trirot(x,y,a,d,dx,dy){
 			var y = Math.random() * canvas.height;
 			var a = Math.random() * 120;
 			var d = 8;
-			var dx = (Math.random() - 0.5) * 0.5;
-			var dy = (Math.random() - 0.5) * 0.5;
+			/*var dx = (Math.random() - 0.5) * 2;
+			var dy = (Math.random() - 0.5) * 2;*/
 			trirotArray.push(new Trirot(x,y,a,d,dx,dy));
 			
 	}
 	 /*console.log(trirotArray);*/
-
+	 c.canvas.addEventListener('mousemove', function(event){
+		 var mouseX = event.clientX - c.canvas.offsetLeft;
+		 var mouseY = event.clientY - $('.holder_sobre_canvas').offset().top;
+		 console.log(mouseX + "MouseX");
+		 console.log(mouseY + "MouseY");
+	 });
 		
 
 
 		function animate(){
 			requestAnimationFrame(animate);
+			
 
 			c.clearRect(0, 0 , innerWidth, innerHeight);
 		/*	for (var i = 0; i < circleArray.length; i++){
@@ -252,6 +304,7 @@ function Trirot(x,y,a,d,dx,dy){
 			}
 		}
 
+		
 		animate();
 
 
